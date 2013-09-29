5f03dda8d716b60c46dfa44aae1d7c3c92e4ec25
tadl <tadl@gmx.de>
2013-09-29 16:43:52 +0200

    autheticity token skipped

--- app/controllers/incoming_message_controller.rb
+++ app/controllers/incoming_message_controller.rb
@@ -1,4 +1,6 @@
 class IncomingMessageController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  
   def create
     ListMailer.send_to_list(List.all).deliver
     redirect_to :users, notice: 'Test Mails send'
