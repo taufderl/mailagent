4ebb28d28be831a1efd16cdc17dafef8826fe92e
tadl <tadl@gmx.de>
2013-10-01 21:46:54 +0200

    stupid error

--- app/controllers/incoming_message_controller.rb
+++ app/controllers/incoming_message_controller.rb
@@ -2,7 +2,7 @@ class IncomingMessageController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def create
-      ListMailer.send_debug_email(params[:message].inspect)
+      ListMailer.send_debug_email(params[:message].inspect).deliver
       render :nothing => true
   end
 end
