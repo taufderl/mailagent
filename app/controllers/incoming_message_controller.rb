d18de4e2a1fa555449cc4573fa6b529ab845d580
tadl <tadl@gmx.de>
2013-10-01 21:40:50 +0200

    error removed

--- app/controllers/incoming_message_controller.rb
+++ app/controllers/incoming_message_controller.rb
@@ -2,7 +2,7 @@ class IncomingMessageController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def create
-      send_debug_email(params[:message].inspect)
+      ListMailer.send_debug_email(params[:message].inspect)
       render :nothing => true
   end
 end
