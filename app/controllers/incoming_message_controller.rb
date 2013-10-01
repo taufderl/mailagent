184e389e32d2d9451b414955251a3d5d6be1d068
tadl <tadl@gmx.de>
2013-10-01 23:28:05 +0200

    incoming message handler version 0.1 works

--- app/controllers/incoming_message_controller.rb
+++ app/controllers/incoming_message_controller.rb
@@ -2,9 +2,33 @@ class IncomingMessageController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def create
-      mail = Mail.new(params[:message])
-      ListMailer.send_debug_email(mail.inspect).deliver
+      #TODO: document, verify and test
+      received_mail = Mail.new(params[:message])
       
+      email = Email.new
+      email.user = User.find_by_email(received_mail.from)
+      email.subject = received_mail.subject
+      email.content = received_mail.text_part.body.decoded
+      
+      List.all.each do |list|
+        email.lists << list if received_mail.to.to_s.include?(list.name)
+      end
+      
+      #User has to be found and in the targetting lists, if not send back error message not
+      if user && (email.list_ids - user.list_ids).empty?
+        #then the mail is valid
+        if email.save
+          #Send mail and be happy
+          ListMailer.send_email(email)
+        else
+          #TODO: Send some email back that can show the error
+          ListMailer.send_debug_email("NOT Successful!\n" + email.inspect).deliver
+        end
+      #if the user could not be found or is not in all lists
+      else
+        #TODO: if the user could not be found or is not in all lists- send error message to the user     
+      end
       render :nothing => true
   end
+  
 end
