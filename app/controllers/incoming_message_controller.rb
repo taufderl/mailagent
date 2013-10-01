ad0b0ff57499ccfd553cef6fd9f115f05f8adc7b
tadl <tadl@gmx.de>
2013-10-01 21:56:53 +0200

    inpect mail object

--- app/controllers/incoming_message_controller.rb
+++ app/controllers/incoming_message_controller.rb
@@ -2,7 +2,9 @@ class IncomingMessageController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def create
-      ListMailer.send_debug_email(params[:message].inspect).deliver
+      mail = Mail.new(params[:message])
+      ListMailer.send_debug_email(mail.inspect).deliver
+      
       render :nothing => true
   end
 end
