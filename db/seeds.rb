b791861fd86286b86b54e6d3210bedac13fb6874
tadl <tadl@taufderl.de>
2015-10-27 23:39:52 +0100

    Remove seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-User.create([{name: 'Tim', email: 'tadl@gmx.de', password: '1234'}])
