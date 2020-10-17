caac6b8b16449c73b28488a28949944f472f018a
Tim auf der Landwehr <tadl-github@taufderl.de>
2020-10-17 13:47:50 +0200

    Fix mail_receiver.rb to not rely on curl

--- script/mail_receiver.rb
+++ script/mail_receiver.rb
@@ -1,23 +1,28 @@
 require 'cgi'
+require 'net/http'
+require 'uri'
+require 'json'
 
 startTime = Time.now
 
-input = "message=#{CGI.escape(STDIN.read)}"
+input = STDIN.read
 
-filename = "/home/kjb/mailagent/log/maildump_#{Time.now.to_i}.log"
-#filename = "./maildump_#{Time.now.to_i}"
-
+filename = "/home/kkb/mailagent/log/maildump_#{Time.now.to_i}_input.log"
 file = File.open filename, 'w'
 file.write input
 file.close
 
-# note the backticks here execute the command
-result = `curl -X POST -d @#{filename} http://localhost:3333/incoming_messages`
-#`curl -X POST -d @#{filename} http://localhost:3000/incoming_messages`
+data = "message=#{CGI.escape(input)}"
+
+filename_data = "/home/kkb/mailagent/log/maildump_#{Time.now.to_i}.log"
+file = File.open filename_data, 'w'
+file.write data
+file.close
+
+result = Net::HTTP.post URI('http://localhost:1337/incoming_messages'), { "message" => input }.to_json, "Content-Type" => "application/json"
 
 endTime = Time.now
 
-logFile = File.open '/home/kjb/mailagent/log/email.log', 'a'
-#logFile = File.open 'email.log', 'a'
-logFile.write "#{startTime}. |#{endTime-startTime}s| #{result}\n"
+logFile = File.open '/home/kkb/mailagent/log/email.log', 'a'
+logFile.write "#{startTime}. |#{endTime-startTime}s| #{result.body}\n"
 logFile.close
