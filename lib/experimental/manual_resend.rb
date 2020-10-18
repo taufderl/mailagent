4f55714915f4e6799dafcfa12fb0101bbd731556
taufderl <tadl-git@taufderl.de>
2020-10-18 02:57:49 +0200

    cleanup

--- lib/experimental/manual_resend.rb
+++
@@ -1,9 +0,0 @@
-
-filename = "./maildump_1383519827.log"
-
-# note the backticks here execute the command
-#`curl -X POST -d @"#{filename}" http://cjg-belm.de/fcgi-bin/mailagent/incoming_messages`
-`curl -X POST -d @#{filename} http://localhost:3000/incoming_messages`
-
-endTime = Time.now
-
